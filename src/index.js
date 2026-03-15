29ddc5359b15709bf396bab3b9ef2113f90c608f
emze <kontakt@marcus-meindel.de>
2026-03-15 15:56:53 +0100

    odin test applied / finished

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import HashMap from "./HashMap.js";
 
 let map = new HashMap()
 
-const originalCapacity = map.capacity
+map.set('apple', 'red')
+map.set('banana', 'yellow')
+map.set('carrot', 'orange')
+map.set('dog', 'brown')
+map.set('elephant', 'gray')
+map.set('frog', 'green')
+map.set('grape', 'purple')
+map.set('hat', 'black')
+map.set('ice cream', 'white')
+map.set('jacket', 'blue')
+map.set('kite', 'pink')
+map.set('lion', 'golden')
+map.set('moon', 'silver')
 
-const firstKeyAmountToGrow = map.capacity * map.loadFactor
-for (let i = 0; i <= firstKeyAmountToGrow; i++) {
-  map.set(`${i}`, i)
-}
+console.log(`
+  map-capacity: ${map.capacity}\n
+  map-length: ${map.length()}\n
+  map-entries: ${JSON.stringify(map.entries(), 0, 2)}
+  `)
+
+
